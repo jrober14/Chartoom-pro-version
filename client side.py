a47050500afda8421c29ce64ae344e9c7cc354aa
jrober14 <35163951+jrober14@users.noreply.github.com>
2018-09-16 11:00:32 +0100

    Update client side.py

--- client side.py
+++ client side.py
@@ -1,13 +1,20 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('Name of computer to connect', 50000))
+s.connect(('192.168.1.72', 50000))
 m = "HI"
 while True:
-    print(1)
-    s.send(m.encode())
-    data = s.recv(1024)
-    print('Received', repr(data))
-    m = input(": ")
-    
+    try:
+        print(1)
+        s.send(m.encode())
+        data = s.recv(1024)
+        reci = repr(data)
+        print(reci)
+        print("The Message Recived is " + rec[2:-1])
+        m = input(": ")
+    except:
+        s.shutdown(1)
+        s.close()
+        print("Quiting")
+        exit()
     
 # socket.gethostbyname_ex(socket.gethostname())[-1]
