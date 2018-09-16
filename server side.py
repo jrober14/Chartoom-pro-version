9bb49b58b4234635b97e772db443a5effd71f496
jrober14 <35163951+jrober14@users.noreply.github.com>
2018-09-16 11:10:30 +0100

    Update server side.py

--- server side.py
+++ server side.py
@@ -1,25 +1,25 @@
 import socket
-q = "hi"
+#q = "hi"
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 50000))
 s.listen(1)
 conn, addr = s.accept()
-print(1)
-print(conn)
-print(2)
-print(addr)
-print(3)
+#print(1)
+#print(conn)
+#print(2)
+#print(addr)
+#print(3)
 while True:
     try:
         data = conn.recv(1024)
         rec = repr(data)
-        print(rec)
-        print(1)
-        print(data)
-        print(2)
+        #print(rec)
+        #print(1)
+        #print(data)
+        #print(2)
         print("The Message Recived is " + rec[2:-1])
-        print(3)
-        data = input(": ")
+        #print(3)
+        data = input("What Message Would You like to send?: ")
         conn.send(data.encode())
     except:
         conn.shutdown(1)
